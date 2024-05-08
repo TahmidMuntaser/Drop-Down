bfa9368cba5633992ae417ff38ea46db9c6544d8
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-05-08 19:18:43 +0600

    refresh prob

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-    document.querySelector('.abus').classList.toggle('move-right');
+// On page load
+// used to restorethe state (sidebar & checkbox)
+window.onload = function() {
+
+    var toggleSidebar = localStorage.getItem('toggleSidebar');
+
+    if (toggleSidebar === null) return;
+
+    var isChecked = (toggleSidebar === 'true');
+    document.getElementById('toggle-sidebar').checked = isChecked; //checkbox state restore
+    document.querySelector('.abus').classList.toggle('move-right', isChecked); //sidebar state restore
+};
+
+// checkbox state change
+document.getElementById('toggle-sidebar').addEventListener('change', function() {
+    localStorage.setItem('toggleSidebar', this.checked);
+    document.querySelector('.abus').classList.toggle('move-right', this.checked);
 });
 
-document.getElementById('toggle-sidebar').addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('open');
-});
+// document.getElementById('toggle-sidebar').addEventListener('click', function() {
+//     document.getElementById('sidebar').classList.toggle('open');
+// });
+
+
+// Event listener for sidebar toggle
+// document.getElementById('toggle-sidebar').addEventListener('click', function() {
+//     var sidebarOpen = document.getElementById('sidebar').classList.contains('open');
+//     document.getElementById('checkbox').checked = sidebarOpen;
+// });
