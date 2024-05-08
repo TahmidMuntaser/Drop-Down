d411c69bf8c493dabf39984a087522db752d3a2b
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2024-05-08 11:19:31 +0600

    upd-1.0.3

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('toggle-sidebar').addEventListener('click', function() {
+    document.querySelector('.abus').classList.toggle('move-right');
+});
+
+document.getElementById('toggle-sidebar').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('open');
+});
